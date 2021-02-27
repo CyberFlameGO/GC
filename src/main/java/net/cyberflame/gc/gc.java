995d5064a1dcc55f5bf088080714bb820a97eb1a
CyberFlame <cyberflameu@gmail.com>
2021-02-27 17:27:09 +1300

    Create gc.java

---
+++ src/main/java/net/cyberflame/gc/gc.java
@@ -0,0 +1,20 @@
+package net.cyberflame.gc;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class GarbageCollector extends JavaPlugin {
+  public void onEnable() {
+    saveDefaultConfig();
+    (new BukkitRunnable()
+      {
+        public void run() {
+          if (GarbageCollector.this.getConfig().getBoolean("debug")) {
+            System.out.println("[GC] Running scheduled garbage collection task...");
+          }
+          System.gc();
+        }
+      }).runTaskTimerAsynchronously((Plugin)this, 0L, getConfig().getLong("frequency") * 20L * 60L);
+  }
+}
